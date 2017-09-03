281837416c9c5f749a4c537943cfb18c51b23ec3
boblee2000 <boblee2000@outlook.com>
2017-09-03 13:30:34 -0400

    now it works well , we can get all of the post from cfg

--- cfc.py
+++ cfc.py
@@ -7,7 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 
 DOWNLOAD_URL = 'http://bbs.comefromchina.com/forums/5/'
-index = 2
+postNumber = 0
 
 
 def download_page(url):
@@ -17,24 +17,28 @@ def download_page(url):
     }).content
 
 def parse_post(html):
+    global postNumber
     soup = BeautifulSoup(html, "lxml")
     title = soup.find('body', attrs={'class': 'node5'}).find('div', attrs={'class': 'titleBar'})
     print "Post_Title ------->[%s]"%title.h1.string
     root = soup.find('ol', attrs={'class':'messageList', 'id':'messageList'})
     i = 0
+    QuoteText = None
     for list in root.find_all('li', attrs={'id': re.compile(r'post-\d+'), 'class':'message'}):
-        i += 1
-        print "Post%d Author:%s"%(i, list['data-author'])
-        quote = list.find_all('div', attrs={'class':'bbCodeBlock'})
-        print "............................................"
-        while quote:
-            
+        postNumber += 1
+        print "Post%d Author:%s"%(postNumber, list['data-author'])
+        message = list.find('div', attrs={'class':'messageContent'})
+        messageText = message.get_text(strip=True)
+        for quote in list.find_all('div', attrs={'class':'bbCodeBlock'}):
+            replacedText = quote.get_text(strip=True)
+            messageText = messageText.replace(replacedText, '') 
+            print "............................................"
             print "Quote from %s" % quote['data-author']
             QuoteText = quote.find('div', attrs={'class':'quote'})
-            print QuoteText.get_text(strip=True)        
-        print "............................................"
-        message = list.find('div', attrs={'class':'messageContent'})
-        print message.get_text('|', strip=True)
+            print QuoteText.get_text(strip=True)
+        if QuoteText:        
+            print "............................................"
+        print messageText
         print "\n\n\n"
     try:
         next = soup.find('div', attrs={'id':'headerMover'}).find('div',attrs={'class':'PageNav'}).find('a',attrs={'href':re.compile(r'threads/\d+/page-\d+'),'class':'text'}, text="下一页 >")
@@ -46,6 +50,7 @@ def parse_post(html):
     else:
         return None
 def parse_html(html):
+    global postNumber
     soup = BeautifulSoup(html, "lxml")
     root = soup.find('div', attrs={'class':'discussionList'})
     leaf = root.find('ol', attrs={'class':'discussionListItems'})
@@ -55,7 +60,8 @@ def parse_html(html):
         print "post-------->"+url
         while url:
             html = download_page(url)
-            url = parse_post(html)    
+            url = parse_post(html)
+        postNumber = 0    
     next = soup.find('div', attrs={'class':'pageNavLinkGroup'}).find('div',attrs={'class':'PageNav'}).find('a',attrs={'href':re.compile(r'forums/\d+/page-\d+'),'class':'text'}, text="下一页 >")
     return "http://bbs.comefromchina.com/"+next['href']
 
