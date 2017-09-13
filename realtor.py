4409c5f2d7299aec9ff0337888ddd25bcd2a1943
boblee2000 <boblee2000@outlook.com>
2017-09-12 22:14:40 -0400

    we can get house info

--- realtor.py
+++ realtor.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 #-*-coding:utf-8 -*-
 
+import json
 import codecs
 import re
 import requests
 from bs4 import BeautifulSoup
 import http
 import random
+import time
 
 DOWNLOAD_URL = 'https://www.realtor.ca/'
 FILTER = 1
@@ -18,7 +20,8 @@ Cookie = {
     "__qca":"P0-790114168-1501392557222", "__utma":"151806763.485722766.1501392556.1503617627.1503623980.2", "__utmc":"151806763", "__utmz":"151806763.1503623980.2.2.utmcsr=bbs.comefromchina.com|utmccn=(referral)|utmcmd=referral|utmcct=/threads/880733/page-4", "xfa_user":"127809%2Ca2949cb8acf1b54363ceb0a9d3f78fb6f517f966", "xfa_session":"8e5844c7732e0e69922da55d811b0099", "_ga":"GA1.2.485722766.1501392556", "_gid":"GA1.2.1655723088.1504142767", "_gat":'1', "__asc":"0a6bbb3b15e4f66d58612d00be2", "__auc":"e76f6d0b15d91f852c2d2c6b194"
 }
 
-proxies = {"https": "185.5.18.164:3128", "https": "23.94.234.42:8080", "https": "45.76.6.86:3128", "https": "35.187.227.175:8080"}  
+proxies = {"https": "123.0.213.100:80", "https": "124.158.124.237:65103", "https": "124.81.234.124:8080", "https": "124.152.84.65:53281", "https": "123.180.133.42:8118", "https":"125.25.55.188:8080"}  
+
 
 UAS = ("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1", 
        "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
@@ -48,15 +51,15 @@ postData = {
     'BedRange':'0-0',
     'BathRange':'0-0',
     'OwnershipTypeGroupId':'2',
-    'LongitudeMin':'-81.86698576416018',
-    'LongitudeMax':'-74.01176115478518',
-    'LatitudeMin':'41.02707659739538',
-    'LatitudeMax':'47.37373046918453',
+    'LongitudeMin':'-76.10138258109129',
+    'LongitudeMax':'-75.71033796439207',
+    'LatitudeMin':'45.220726108171895',
+    'LatitudeMax':'45.40566522481841',
     'SortOrder':'A',
     'SortBy':'1',
     'viewState':'m',
-    'Longitude':'-75.94226499999999',
-    'Latitude':'45.318620499999994',
+    'Longitude':'-75.94229539999999',
+    'Latitude':'45.318612599999994',
     'CurrentPage':'1',
     'ZoomLevel':'7',
     'PropertyTypeGroupID':'1',
@@ -65,7 +68,6 @@ postData = {
     'Version':'6.0'
 }
 
-kkk = " ApplicationSettings.Token = 'D6TmfZprLI+bju9lrx4jNjGK46lLSsV+jM3lrScYgjc='; ApplicationSettings.GUID = '2417a064-24b2-4cf8-8b85-a00ba018b82b';    "
 
 def postDataToGetPage(loginUrl, postData):
     global session
@@ -85,47 +87,46 @@ def parse_html(html):
     global postNumber,publisher, session
 
     soup = BeautifulSoup(html, "lxml")
-    root = soup.find('div', attrs={'class':'discussionList'})
-    leaf = root.find('ol', attrs={'class':'discussionListItems'})
-    for list in leaf.find_all('li', attrs={'id':re.compile(r'thread-\d+')}):
-        _, _, id = list['id'].partition('-')
-        url = "http://bbs.comefromchina.com/threads/"+id
-        if FILTER and list['data-author'] not in publisher:
-            continue
-        print "post-------->"+url
-        while url:
-            html = download_page(url, session)
-            url = parse_post(html)
-        postNumber = 0    
-    next = soup.find('div', attrs={'class':'pageNavLinkGroup'}).find('div',attrs={'class':'PageNav'}).find('a',attrs={'href':re.compile(r'forums/\d+/page-\d+'),'class':'text'}, text="下一页 >")
-    return "http://bbs.comefromchina.com/"+next['href']
+    print soup
+    root = soup.find('div', attrs={'class':'m_map'}).find('div', attrs={'id':'listView'})
+    for list in root.find_all('div', attrs={'class':'m_property_lst_row'}):
+        print list.text
+    return None
 
 def parse_mainPage(html):
-    global postData
-    print html
-    #soup = BeautifulSoup(html, "lxml")
-    #root = soup.find('head', attrs={'id':'elHead'})
-    #print root
-    m = re.match(r"ApplicationSettings\.Token = \'(.*?)\'", kkk)
-    token = m.groups()[0]
-    print token
-    n = re.match(r"ApplicationSettings\.GUID", kkk)
-    guid = n.groups()[0]
-    print guid
-    #print root.text
+    soup = BeautifulSoup(html, "lxml")
+    root = soup.find('head', attrs={'id':'elHead'})
+    keyList = re.findall(r"ApplicationSettings\.(?P<name>\w+) = \'(?P<value>.*?)\'", root.text)
+    keyDict = dict(keyList)
+    print keyDict, '\n\n\n'
+    return keyDict
 
 def main():
-    global session
+    global session,postData
     url = DOWNLOAD_URL
-    #html = download_page(url)
-    parse_mainPage(None) 
-'''
-    session = postDataToGetPage(loginUrl, postData)
+    html = download_page(url)
+    #print html
+    keyDict = parse_mainPage(html) 
+    postData['Token']=keyDict['Token']
+    postData['GUID']=keyDict['GUID']
+    #print postData
+        
+    while postData:
+        html = session.post(loginUrl, data=postData, headers=headers)
+        if not html: break
+                
+        kk = json.loads(html.text)
+        print kk['Paging']
+        currentPage = kk['Paging']['CurrentPage']
+        totalPages = kk['Paging']['TotalPages']
+        
+        postData['CurrentPage']= str(currentPage + 1)
+        print postData['CurrentPage']
+        if currentPage >= totalPages:
+            postData = None
+        time.sleep(3)        
+        #postData = parse_html(html)
 
-    while url:
-        html = download_page(url, session)
-        url = parse_html(html)
-'''
 if __name__ == '__main__':
     main()
 
